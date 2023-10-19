70d745901f77ef25b4421c7b35e0a3980b279735
Livarbesaru <liburdidaniele1998@gmail.com>
2023-10-19 15:09:13 +0200

    feat (Properties): add class to read properties from json files

---
+++ utils/properties.js
@@ -0,0 +1,30 @@
+const fileReader = require("./fileReader")
+const {error} = require("next/dist/build/output/log");
+
+class Properties{
+    map;
+    constructor() {
+        this.map = new Map();
+    }
+
+    async addProperties(dir,file){
+        return new Promise((res,rej)=>{
+            fileReader.readFileFromPath(dir,file).then(response => {
+                if(response.code === 200){
+                    const {data,type,code,name} = response;
+                    let parsedData = JSON.parse(data.toString('utf8'));
+                    this.map.set(name,parsedData);
+                    res(`Properties file ${name} has been loaded`);
+                    console.log(`file ${file} has been loaded in Properties =>`, parsedData);
+                }else{
+                    rej('fuck properties');
+                }
+            });
+        })
+    }
+    getProperties(properties){
+        return this.map.get(properties);
+    }
+}
+
+module.exports = new Properties();
