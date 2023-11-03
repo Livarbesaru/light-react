86c7bb985b56473f1d74c4b1cf4112ed8377d232
Livarbesaru <liburdidaniele1998@gmail.com>
2023-11-03 14:10:52 +0100

    refactor (Datasource): get properties with destructuring

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ propertiesReader.addProperties("properties","properties.json").then(res =>{
     console.log(res)
     const generalProperties = propertiesReader.getProperties("properties");
     if(generalProperties != null){
-        let port = generalProperties["server"]["port"];
-        let hostname = generalProperties["server"]["hostname"];
+        let [port,hostname] = [generalProperties["server"]["port"],generalProperties["server"]["hostname"]];
         appServer.listen(port,hostname,()=>{
             console.log(`Server started on port:${port} with hostname:${hostname}`)
         })
