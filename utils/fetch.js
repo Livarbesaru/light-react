70c893978e6b0a52f88a6e7956529b61502d1fdd
Livarbesaru <liburdidaniele1998@gmail.com>
2024-07-12 13:57:41 +0200

    ref: center manipolation of buffer tp json by http in the fetch component

--- utils/fetch.js
+++ utils/fetch.js
@@ -10,31 +10,27 @@ const options = {
 */
 async function fetch(options, httpsOn = false) {
     let body = [];
-    let req = { end: () => { console.log("end nothing"); } };
     const protocol = httpsOn ? https : http;
 
     try {
-        await new Promise((resolve, reject) => {
-            req = protocol.request(options, (response) => {
-                response.on('error', (err) => {
-                    reject("not ok")
-                    throw new Error(`Errore nella risposta: ${err.message}`);
-                })
-                .on('data', (chunk) => {
-                    body.push(chunk);
-                })
-                .on('end', () => {
-                    if (body.length > 0) {
-                        body = JSON.parse(Buffer.concat(body).toString());
-                    } else {
-                        body = {};
-                    }
-                    resolve("ok");
-                });
+        await new Promise ((res,rej)=>{
+            req = protocol.request(options, async (response) => {
+                try{
+                    body = await manipolateJson(response, body);
+                    res("ok");
+                }catch(error){
+                    console.error(error);
+                    rej("not ok")
+                }
             });
 
+            req.on('error', (err) => {
+                console.error('Request error:', err);
+                rej(err);
+            });
+    
             req.end();
-        });
+        })
     } catch (err) {
         console.error(err);
         body = {};
@@ -43,4 +39,25 @@ async function fetch(options, httpsOn = false) {
     return body;
 }
 
-module.exports = fetch;
+async function manipolateJson(mediator, body) {
+    await new Promise((res, rej) => {
+        mediator.on('error', (err) => {
+            rej("not ok")
+            throw new Error(`Errore nella risposta: ${err.message}`);
+        })
+            .on('data', (chunk) => {
+                body.push(chunk);
+            })
+            .on('end', () => {
+                if (body.length > 0) {
+                    body = JSON.parse(Buffer.concat(body).toString());
+                } else {
+                    body = {};
+                }
+                res("ok");
+            });
+    });
+    return body;
+}
+
+module.exports = [fetch, manipolateJson];
--- utils/requestMarshall.js
+++ utils/requestMarshall.js
@@ -1,36 +1,25 @@
 const Request = require("../model/request");
 const url = require('url');
+const [fetch, manipolateJson] = require("./fetch");
 
 
-async function marshallRequest(request){
-    const urlParsed = url.parse(request.url)
-    const query = [...(urlParsed.query!=null ? urlParsed.query : "").split("&")];
-    const queryAsMap = new Map();
-    for(paramValue of query){
-        let [first, ...rest] = paramValue.split('=')
-        rest = rest.join('=');
-        queryAsMap.set(first,rest);
-    }
-    let body = [];
-    await new Promise((res,rej)=>{
-      request.
-        on('error', err => {
-          rej("not ok");
-          throw new Error("error while reading body %s",err)
-        })
-        .on('data', chunk => {
-          body.push(chunk);
-        })
-        .on('end', () => {
-          if(body.length > 0){
-            body = JSON.parse(Buffer.concat(body).toString());
-          }else{
-            body = {};
-          }
-          res("ok");
-      });
-    })
-    return new Request(urlParsed.pathname,request.method.toUpperCase(),queryAsMap,body,request.headers,{});
+async function marshallRequest(request) {
+  const urlParsed = url.parse(request.url)
+  const query = [...(urlParsed.query != null ? urlParsed.query : "").split("&")];
+  const queryAsMap = new Map();
+  for (paramValue of query) {
+    let [first, ...rest] = paramValue.split('=')
+    rest = rest.join('=');
+    queryAsMap.set(first, rest);
+  }
+  let body = [];
+  try {
+    body = await manipolateJson(request, body);
+  } catch (error) {
+    console.error(error);
+    body = {};
+  }
+  return new Request(urlParsed.pathname, request.method.toUpperCase(), queryAsMap, body, request.headers, {});
 }
 
 module.exports = marshallRequest;
